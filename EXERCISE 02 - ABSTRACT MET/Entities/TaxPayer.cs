8c2d9d7677dcae1205d549779a95212d8ea172af
Marcus Bianchi <82729031+CmdBianchi@users.noreply.github.com>
2021-05-11 16:57:43 -0300

    Adicionar arquivos de projeto.

---
+++ EXERCISE 02 - ABSTRACT MET/Entities/TaxPayer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCISE_02___ABSTRACT_MET.Entities {
+    //------------------------------- START -------------------------------//
+    abstract class TaxPayer {
+
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        //---------> CONSTRUCTOR
+        public TaxPayer() {
+        }
+        public TaxPayer(string name, double anualIncome) {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        //---------> MET
+        public abstract double Tax();
+
+    }
+    //-------------------------------- END -------------------------------//
+}
